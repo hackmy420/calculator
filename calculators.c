540f483fd4bec9a98a43361e940596b13f801d46
Mayukh Saha <mayukh960@gmail.com>
2021-10-07 15:21:50 +0530

    created calculator c file using switch case

---
+++ calculators.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{  
+     char op;
+   printf("enter operator\n");
+   scanf("%c",&op);
+
+      float Num1;
+    printf("enter NUM1\n");
+    scanf("%f",&Num1);
+    
+    
+   
+   float Num2;
+   printf("enter NUM2\n");
+    scanf("%f",&Num2);
+   float result;
+
+   switch (op)
+   {
+   case '+':
+    result = Num1+Num2;
+    printf("result is : %f",result);
+       break;
+   
+   case '-':
+   result = Num1-Num2;
+   printf("result is : %f",result);
+   break;
+
+   case '*':
+   result = Num1*Num2;
+   printf("result is : %f",result);
+   break;
+
+   case '/':
+   result = Num1/Num2;
+   printf("result is : %f",result);
+   break;
+
+    default:
+      printf("INVALID OPERATOR");
+       break;
+   }    
+ 
+ return 0;
+}
