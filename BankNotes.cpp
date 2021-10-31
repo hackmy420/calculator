569788e97192f986e029f9fbd182de4eae9d3cb4
PradeepSingh2004 <93465217+PradeepSingh2004@users.noreply.github.com>
2021-10-31 13:01:59 +0530

    Create BankNotes.cpp

---
+++ BankNotes.cpp
@@ -0,0 +1,270 @@
+/*
+C. Banknotes
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+In Berland, n different types of banknotes are used. Banknotes of the i-th type have denomination 10ai burles (burles are the currency used in Berland); the denomination of banknotes of the first type is exactly 1.
+
+Let's denote f(s) as the minimum number of banknotes required to represent exactly s burles. For example, if the denominations of banknotes used in Berland are 1, 10 and 100, then f(59)=14: 9 banknotes with denomination of 1 burle and 5 banknotes with denomination of 10 burles can be used to represent exactly 9⋅1+5⋅10=59 burles, and there's no way to do it with fewer banknotes.
+
+For a given integer k, find the minimum positive number of burles s that cannot be represented with k or fewer banknotes (that is, f(s)>k).
+
+Input
+The first line contains a single integer t (1≤t≤104) — number of test cases.
+
+The first line of each test case contains two integers n and k (1≤n≤10;1≤k≤109).
+
+The next line contains n integers a1,a2,…,an (0=a1<a2<⋯<an≤9).
+
+Output
+For each test case, print one integer — the minimum positive number of burles s that cannot be represented with k or fewer banknotes.
+
+Example
+inputCopy
+4
+3 13
+0 1 2
+2 777
+0 4
+3 255
+0 1 3
+10 1000000000
+0 1 2 3 4 5 6 7 8 9
+outputCopy
+59
+778
+148999
+999999920999999999
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<ll>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef queue<ll>           qll;
+typedef vector<plll>      vplll;
+typedef  vector<set<ll>>   vsll;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<vll, ll>             mvlll;
+typedef  map<char, ll>           mcl;
+typedef map<pair<ll, ll>, ll>  mplll;
+typedef  unordered_map<char, ll>           umcl;
+typedef  unordered_map< ll, char>           umlc;
+typedef  unordered_map< ll, ld>           umld;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+typedef priority_queue<ll, vector<ll>, greater<ll>> prq;
+const ll MOD = 1000000007;
+const ll modx = 998244353;
+ld PI = 3.1415926535897;
+const ll N = 200010;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+	ll t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+ll powmod(ll x, ll y) {
+	ll res = 1;
+	for (ll i = 0; i < y; i++) {
+		res = res * x % MOD;
+	}
+	return res;
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+bool COMP(pll l, pll r) {
+	return l.ss < r.ss;
+}
+ll kadanesAlgo(vll a)
+{
+	ll n = a.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	foi(n)
+	{
+		currMax += a[i];
+
+		if (currMax <= a[i])
+		{
+			currMax = a[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
+ll ask(ll x, ll y, ll n) {
+	cout << "?" << " ";
+	foi(n - 1) {
+		cout << x << " ";
+	}
+	cout << y << endl;
+	ll s;
+	cin >> s;
+	return s;
+}
+ll binpow(ll a, ll b, ll m) {
+	a %= m;
+	ll res = 1;
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
+	return res;
+}
+bool is_sorted(vector<ll>a) {
+	ll x = 0;
+	for (ll i = 0; i < a.size() - 1; i++) {
+		if (a[i] < a[i + 1])
+			x++;
+	}
+	return x == a.size() - 1;
+}
+bool check(string a, string b) {
+	ll n = a.size(), m = b.size();
+	if (a < b) {
+		return true;
+	}
+	if (a > b) {
+		return false;
+	}
+	foi(a.size()) {
+		if (a[i] > b[i]) {
+			return false;
+		}
+		else if (a[i] < b[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+bool isSubSequence(string a, string b, ll m, ll n) {
+	ll j = 0;
+	for (ll i = 0; i < n and j < m; i++)
+		if (a[j] == b[i])
+			j++;
+	return (j == m);
+}
+ll power(ll n, ll x) {
+	ll ans = 1;
+	foi(n) {
+		ans *= x;
+	}
+	return ans;
+}
+void solve() {
+	ll n, k, ans = 0;
+	cin >> n >> k;
+	vll a(n, 0);
+	foi(n) {
+		cin >> a[i];
+	}
+	ll x = a[n - 1], y = k + 1;
+	foi(n - 1) {
+		if (y) {
+			ll p = power(a[i + 1] - a[i], 10), r = power(a[i], 10);
+			ans += r * min(p - 1, y);
+			y -= min(p - 1, y);
+		}
+	}
+	ll p = power(x, 10);
+	cout << ans + y * p;
+}
+
+
+ 
