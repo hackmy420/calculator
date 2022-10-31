a62e9ebb6d11c3d4105684bbf98e946e1c156f6a
hackmy420 <72398623+hackmy420@users.noreply.github.com>
2022-10-31 19:43:53 +0530

    Update CalculatorPy.py

--- CalculatorPy.py
+++ CalculatorPy.py
@@ -24,9 +24,10 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Moduler")
 
 while True:
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
